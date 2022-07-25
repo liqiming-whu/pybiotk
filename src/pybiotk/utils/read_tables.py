2a08b14d7f5a9f73c09ae2af5dcb2d006315c71d
liqiming_whu <liqiming@whu.edu.cn>
2022-07-25 16:43:17 +0800

    update

--- src/pybiotk/utils/read_tables.py
+++ src/pybiotk/utils/read_tables.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+A simple tool for joining and filtering tables
+"""
 import argparse
 import sys
 
@@ -7,7 +10,7 @@ import pandas as pd
 from pybiotk.utils import read_table, write_table, ignore
 
 
-def main(table_list, outfile, namefile, noheader, column, exclude=False):
+def main(table_list, outfile, namefile, noheader, column, exclude=False, contains=False):
     df_list = []
     for table in table_list:
         header = None if noheader else 0
@@ -15,9 +18,15 @@ def main(table_list, outfile, namefile, noheader, column, exclude=False):
         if namefile is not None:
             names = set(j for i in namefile for j in i.split())
             if exclude:
-                df = df.loc[~df.iloc[:, column].isin(names)]
+                if contains:
+                    df = df.loc[~df.iloc[:, column].str.contains("|".join(names))]
+                else:
+                    df = df.loc[~df.iloc[:, column].isin(names)]
             else:
-                df = df.loc[df.iloc[:, column].isin(names)]
+                if contains:
+                    df = df.loc[df.iloc[:, column].str.contains("|".join(names))]
+                else:
+                    df = df.loc[df.iloc[:, column].isin(names)]
         df_list.append(df)
     if len(df_list) == 1:
         out_df = df_list[0]
@@ -34,14 +43,15 @@ def run():
     parser.add_argument("input", type=str, nargs="+",
                         help="Input tables.")
     parser.add_argument('-o', dest='output', type=argparse.FileType('w'),
-                        default=sys.stdout, help="output file name.")
+                        default=sys.stdout, help="output file name [stdout]")
     parser.add_argument('-n', dest="namefile", type=argparse.FileType('r'), default=(None if sys.stdin.isatty() else sys.stdin),
-                        help="whose name is listed in FILE.")
+                        help="whose name is listed in FILE|stdin")
     parser.add_argument('-H', "--noheader", dest="noheader", action="store_true", help="if noheader")
     parser.add_argument('-c', dest="column", type=int, default=0, help="name column")
-    parser.add_argument('-e', dest="exclude", action="store_true", help="name is not listed in FILE.")
+    parser.add_argument('-e', dest="exclude", action="store_true", help="name is not listed in FILE|stdin")
+    parser.add_argument('--contains', dest="contains", action="store_true", help="contains one of a substrings listed in FILE|stdin.")
     args = parser.parse_args()
-    main(args.input, args.output, args.namefile, args.noheader, args.column, args.exclude)
+    main(args.input, args.output, args.namefile, args.noheader, args.column, args.exclude, args.contains)
 
 
 if __name__ == "__main__":
