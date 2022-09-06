03f032347335cce5942f751c7a16a787ef20f49a
liqiming-whu <liqiming@whu.edu.cn>
2022-09-06 15:41:36 +0800

    update

--- src/pybiotk/utils/read_tables.py
+++ src/pybiotk/utils/read_tables.py
@@ -10,7 +10,7 @@ import pandas as pd
 from pybiotk.utils import read_table, write_table, ignore
 
 
-def main(table_list, outfile, namefile, noheader, column, exclude=False, contains=False):
+def main(table_list, outfile, namefile, noheader, column, delimiter=None, exclude=False, contains=False):
     df_list = []
     for table in table_list:
         header = None if noheader else 0
@@ -21,12 +21,18 @@ def main(table_list, outfile, namefile, noheader, column, exclude=False, contain
                 if contains:
                     df = df.loc[~df.iloc[:, column].str.contains("|".join(names))]
                 else:
-                    df = df.loc[~df.iloc[:, column].isin(names)]
+                    if delimiter is not None:
+                        df = df.loc[~df.iloc[:, column].str.split(delimiter).apply(lambda x: any(set(x) & names))]
+                    else:
+                        df = df.loc[~df.iloc[:, column].isin(names)]
             else:
                 if contains:
                     df = df.loc[df.iloc[:, column].str.contains("|".join(names))]
                 else:
-                    df = df.loc[df.iloc[:, column].isin(names)]
+                    if delimiter is not None:
+                        df = df.loc[df.iloc[:, column].str.split(delimiter).apply(lambda x: any(set(x) & names))]
+                    else:
+                        df = df.loc[df.iloc[:, column].isin(names)]
         df_list.append(df)
     if len(df_list) == 1:
         out_df = df_list[0]
@@ -41,7 +47,7 @@ def run():
         description=__doc__,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("input", type=str, nargs="+",
-                        help="Input tables.")
+                        help="input tables.")
     parser.add_argument('-o', dest='output', type=argparse.FileType('w'),
                         default=sys.stdout, help="output file name [stdout]")
     parser.add_argument('-n', dest="namefile", type=argparse.FileType('r'), default=(None if sys.stdin.isatty() else sys.stdin),
@@ -49,9 +55,10 @@ def run():
     parser.add_argument('-H', "--noheader", dest="noheader", action="store_true", help="if noheader")
     parser.add_argument('-c', dest="column", type=int, default=0, help="name column")
     parser.add_argument('-e', dest="exclude", action="store_true", help="name is not listed in FILE|stdin")
+    parser.add_argument('-d', dest="delimiter", type=str, default=None, help="name is separated by delimiter in input tables")
     parser.add_argument('--contains', dest="contains", action="store_true", help="contains one of a substrings listed in FILE|stdin.")
     args = parser.parse_args()
-    main(args.input, args.output, args.namefile, args.noheader, args.column, args.exclude, args.contains)
+    main(args.input, args.output, args.namefile, args.noheader, args.column, args.delimiter, args.exclude, args.contains)
 
 
 if __name__ == "__main__":
